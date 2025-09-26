714bce4ae1275d0c74190e9cff2f0a9d5dc37c2c
9thCore <38700349+9thCore@users.noreply.github.com>
2025-09-26 16:02:46 +0300

    Register helper and component

--- src/main/java/com/core/kubejsvoltaic/integration/KubeJSVoltaicPlugin.java
+++ src/main/java/com/core/kubejsvoltaic/integration/KubeJSVoltaicPlugin.java
@@ -1,7 +1,19 @@
 package com.core.kubejsvoltaic.integration;
 
+import com.core.kubejsvoltaic.recipe.component.probable.item.ProbableItemRecipeComponent;
+import com.core.kubejsvoltaic.wrapper.ProbableItemWrapper;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
+import dev.latvian.mods.kubejs.recipe.schema.RecipeComponentFactoryRegistry;
+import dev.latvian.mods.kubejs.script.BindingRegistry;
 
 public class KubeJSVoltaicPlugin implements KubeJSPlugin {
+    @Override
+    public void registerRecipeComponents(RecipeComponentFactoryRegistry registry) {
+        registry.register(ProbableItemRecipeComponent.PROBABLE_ITEM);
+    }
 
+    @Override
+    public void registerBindings(BindingRegistry bindings) {
+        bindings.add("VoltaicProbableItem", ProbableItemWrapper.class);
+    }
 }
