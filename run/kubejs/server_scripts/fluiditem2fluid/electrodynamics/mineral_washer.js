2f68ce4acc274ca345d66b2c4955240f30b6e971
9thCore <38700349+9thCore@users.noreply.github.com>
2025-09-27 17:09:10 +0300

    mineral washer

---
+++ run/kubejs/server_scripts/fluiditem2fluid/electrodynamics/mineral_washer.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+    event.recipes.electrodynamics.mineral_washer_recipe(
+        "750x minecraft:water",
+        "2x cobblestone",
+        "250x minecraft:lava"
+    )
+    .ticks(100)
+    .usagePerTick(500);
+});
