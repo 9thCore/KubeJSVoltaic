a5a725d60b4bac188e8de0ece98689d08352f1cd
9thCore <38700349+9thCore@users.noreply.github.com>
2025-09-27 17:09:05 +0300

    fermentation plant

---
+++ run/kubejs/server_scripts/fluiditem2fluid/electrodynamics/fermentation_plant.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+    event.recipes.electrodynamics.fermentation_plant_recipe(
+        "270x electrodynamics:fluidethanol",
+        "4x fermented_spider_eye",
+        "250x electrodynamics:fluidethanol"
+    )
+    .ticks(100)
+    .usagePerTick(500);
+});
