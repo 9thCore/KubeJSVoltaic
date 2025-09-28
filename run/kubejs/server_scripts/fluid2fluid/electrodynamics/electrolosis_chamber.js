147900cdd1354629d9f0b07ac5ef4bc6ec469f45
9thCore <38700349+9thCore@users.noreply.github.com>
2025-09-28 11:49:12 +0300

    electrol(o/y)sis chamber

---
+++ run/kubejs/server_scripts/fluid2fluid/electrodynamics/electrolosis_chamber.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+    // these amounts only really change what's shown in JEI,
+    // and how much fluid there must be in the input. (if "2x minecraft:water" for the input, then at least 2 units of water must be present before conversion happens)
+    // but it's still a 1:1 conversion rate!
+    // (nothing special happens if output is increased)
+    event.recipes.electrodynamics.electrolosis_chamber_recipe(
+        "1x minecraft:lava",
+        "1x minecraft:water"
+    );
+});
