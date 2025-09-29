e7cbef25337a3d35f85dc506df5aca07e7390746
9thCore <38700349+9thCore@users.noreply.github.com>
2025-09-29 10:07:57 +0300

    oops

--- run/kubejs/server_scripts/fluid2gas/electrodynamics/electrolytic_separator.js
+++ run/kubejs/server_scripts/fluid2gas/electrodynamics/electrolytic_separator.js
@@ -1,14 +1,12 @@
 ServerEvents.recipes(event => {
-    for (let i = 0; i < 1; i++) {
-        event.recipes.electrodynamics.electrolytic_separator_recipe(
-            "250x electrodynamics:hydrogen",
-            "250x lava"
-        )
-        // defaults to 1000mB of hydrogen gas at room temperature and sea level pressure (limitation, recipe requires two gasses)
-        // use .gasByproducts("") to use an empty gas!
-        .gasByproducts("")
-        //.gasByproducts("15% electrodynamics:hydrogen") // chances don't seem to work fully for this one
-        .ticks(60)
-        .usagePerTick(250);
-    }
+    event.recipes.electrodynamics.electrolytic_separator_recipe(
+        "250x electrodynamics:hydrogen",
+        "250x lava"
+    )
+    // defaults to 1000mB of hydrogen gas at room temperature and sea level pressure (limitation, recipe requires two gasses)
+    // use .gasByproducts("") to use an empty gas!
+    .gasByproducts("")
+    //.gasByproducts("15% electrodynamics:hydrogen") // chances don't seem to work fully for this one
+    .ticks(60)
+    .usagePerTick(250);
 });
