f3b0381ce720331d8cd1988b3fc3d7749265c25f
9thCore <38700349+9thCore@users.noreply.github.com>
2025-09-29 10:11:04 +0300

    test gas replacement

--- run/kubejs/server_scripts/fluid2gas/electrodynamics/electrolytic_separator.js
+++ run/kubejs/server_scripts/fluid2gas/electrodynamics/electrolytic_separator.js
@@ -9,4 +9,10 @@ ServerEvents.recipes(event => {
     //.gasByproducts("15% electrodynamics:hydrogen") // chances don't seem to work fully for this one
     .ticks(60)
     .usagePerTick(250);
+
+    // water -> 3x argon + 2x hydrogen (lol)
+    event.replaceOutput({
+        mod: "electrodynamics",
+        type: "electrodynamics:electrolytic_separator_recipe"
+    }, "electrodynamics:oxygen", "3x electrodynamics:argon");
 });
