de28fa99c00d7ffb99725a5a8c49c2f310d75562
9thCore <38700349+9thCore@users.noreply.github.com>
2025-09-29 22:25:25 +0300

    Test GasIngredient

--- run/kubejs/server_scripts/general/gas.js
+++ run/kubejs/server_scripts/general/gas.js
@@ -1,5 +1,5 @@
 ServerEvents.loaded(event => {
-    const stack = VoltaicGasStack.of("electrodynamics:hydrogen");
+    let stack = VoltaicGasStack.of("electrodynamics:hydrogen");
     console.log(`We have ${stack}`);
 
     stack.heat(100);
@@ -8,9 +8,23 @@ ServerEvents.loaded(event => {
     stack.setTemperature(100);
     console.log(`Set temperature to 100K, now it's ${stack}`);
 
-    const stack2 = VoltaicGasStack.of("electrodynamics:oxygen");
+    let stack2 = VoltaicGasStack.of("electrodynamics:oxygen");
     console.log(`We have second gas ${stack2}`);
 
     stack2.setPressure(50);
     console.log(`Set pressure to 50 ATM, now it's ${stack2}`);
+
+
+    let ingredient = VoltaicGasIngredient.of("750x #c:nitrogen");
+    console.log(`We have first ingredient ${ingredient}`);
+
+    let ingredient2 = ingredient.withPressure(5);
+    console.log(`Copied to second ingredient with 5 ATM, now it's ${ingredient2}`);
+
+    let ingredient3 = ingredient2.withTemperature(1900);
+    console.log(`Copied to third ingredient with 1900 K, which is ${ingredient3}`);
+
+    let ingredient4 = VoltaicGasIngredient.of("electrodynamics:hydrogen", 1500, 980, 17);
+
+    console.log(`We have fourth, new ingredient ${ingredient4}`);
 });
